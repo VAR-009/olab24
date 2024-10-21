2e0e0bfac58fdc10c4f56b9c7c19282a119419d8
VAR-009 <venkata_b220071cs@nitc.ac.in>
2024-10-21 17:29:55 +0530

    Update Comic.java

--- DOP/src/oldshelf/Comic.java
+++ DOP/src/oldshelf/Comic.java
@@ -2,34 +2,40 @@ package oldshelf;
 
 public class Comic extends Book {
 
-	/* TODO: Add most strict modifiers here*/ private String Title;
+	private String title;
 	private final int ageOfMainCharacter;
 	
 
-	// TODO Correct the error
-	public Comic() {
-			
+	public Comic(String title, int ageOfMainCharacter) {
+		this.title = title;
+		this.ageOfMainCharacter = ageOfMainCharacter;
 	}
 	
 	// TODO : create a getter if required.
-	
-	// TODO: Create a setter if required
+	public String getTitle() {
+		return title;
+	}
+	public int getAge() {
+		return ageOfMainCharacter;
+	}
 
-	// TODO: write a toString method
+	
 	@Override
 	public String toString() {
-		return null;
+		return "Title is " + this.getTitle() + " Age of main character is " + this.getAge() + "\n";
 	}
 
 	// TODO: Bonus: 
 	@Override
 	public int hashCode() {
 		// TODO: Fill up an arbitrary hash method that takes into account only the age of main characted and the Strign title
+		return Integer.hashCode(ageOfMainCharacter);
 	}
 	
 	// TODO: Bonus: 
 	@Override
 	public boolean equals(Object o) {
 		// TODO: Based on the information about hashCode write the equals method.
+		return hashCode() == o.hashCode();
 	}
 }
