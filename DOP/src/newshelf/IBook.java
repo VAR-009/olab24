fa4ca8808a77b5cea5635bf693d2d7bab69620f2
VAR-009 <venkata_b220071cs@nitc.ac.in>
2024-10-25 13:19:03 +0530

    Update IBook.java

--- DOP/src/newshelf/IBook.java
+++ DOP/src/newshelf/IBook.java
@@ -1,5 +1,11 @@
 package newshelf;
+import oldshelf.FictionType;
 
-public interface IBook {
+public sealed interface IBook permits Comic, Fiction, TextBook {}
 
-}
+record Comic(String title,int ageOfMainCharacter) implements IBook{}
+
+record Fiction(String name, FictionType f) implements IBook{}
+
+record TextBook(String subject) implements IBook{}
+
